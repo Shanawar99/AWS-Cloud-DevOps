5593c46a67f9fc8f937b324ab1062a51f3b1a37a
EC2 Default User <ec2-user@ip-172-31-38-191.us-east-2.compute.internal>
2022-01-19 06:00:11 +0000

    a

--- Adeel/Sprint5/AdeelProject5/adeel_project5/adeel_project5_stack.py
+++ Adeel/Sprint5/AdeelProject5/adeel_project5/adeel_project5_stack.py
@@ -97,7 +97,7 @@ class AdeelProject5Stack(cdk.Stack):
         image=ecs.EcrImage(repo, "latest")
 
         # Create an ECS cluster
-        vpc = ec2.Vpc(self, "adeelVip",cidr="10.0.0.0/28",max_azs=1)
+        vpc = ec2.Vpc(self, "adeelVip",cidr="10.0.0.0/24",max_azs=1)
         ''''
         cluster = ecs.Cluster(self, "adeelC",vpc=vpc)
         # Add capacity to it
