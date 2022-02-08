59f993f7bd33762ba0a0fa279aebebdc343ef10c
Shanawar <shanawar.ali.chouhdry.s@skipq.org>
2022-02-08 15:36:09 +0500

    final commit

--- shanawar/Leetcode/1051. Height Checker.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def heightChecker(self, heights):
-        """
-        :type heights: List[int]
-        :rtype: int
-        """
-        return sum(h1 != h2 for h1, h2 in zip(heights, sorted(heights)))
